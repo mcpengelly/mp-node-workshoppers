7ec7dcd00ddc507d8751818ac44cbfb9798a6918
Matthew Pengelly <pengelly.mat@gmail.com>
2015-12-06 23:18:39 -0500

    timeserver implemented

--- learnyounode/http-file-server/httpfileserver.js
+++ learnyounode/http-file-server/httpfileserver.js
@@ -1,1 +1,10 @@
 var http = require('http');
+var fs = require('fs');
+var port = Number(process.argv[2]);
+var pathToFile = process.argv[3];
+
+//create a server that serves the same file to
+http.createServer(function(req, res) {
+	res.writeHead(200, { 'content-type': 'text/plain' });
+	var readStream = fs.createReadStream(pathToFile).pipe(res);
+}).listen(port);
