59ed5128b80a8c2d5e69e3ce5c56515d592c9d9c
Matthew <pengelly.mat@gmail.com>
2015-12-13 00:11:35 -0500

    sleep break

---
+++ streamadventure/lines/lines.js
@@ -0,0 +1,17 @@
+var split = require('split');
+var through2 = require('through2');
+var lineCount = 0;
+
+process.stdin.pipe(split()).pipe(through2(function(line, _, next) {
+	if (lineCount % 2 === 0) {
+			this.push(line
+				.toString()
+				.toLowerCase() + '\n');
+		} else {
+			this.push(line
+				.toString()
+				.toUpperCase() + '\n');
+	}
+	lineCount++;
+	next();
+})).pipe(process.stdout);
