eb994373dcc139944fdfcf720d04ecea78e292ff
Matthew <pengelly.mat@gmail.com>
2015-12-20 00:01:26 -0500

    started everyone is min

--- lololodash/everyone-is-min/everyoneismin.js
+++ lololodash/everyone-is-min/everyoneismin.js
@@ -1,9 +1,25 @@
 var _ = require('lodash');
+var result = {
+	hot: [],
+	warm: []
+};
+
+var sortHot = function (item) {
+	return item.temperature >= 19;
+};
+
+var sortWarm = function (item) {
+	return item.temperature >= 19;
+};
 
 var worker = function (collection) {
- _.every();
- _.some();
- // might be best to use filter
+	console.log(_.every(collection, sortHot));
+	if(_.every(collection, sortHot)) {
+		result.hot.push('asdasd');
+	} else if (_.some(collection, sortWarm)) {
+		result.warm.push('dasd');
+	}
+	return result;
 };
 
 module.exports = worker;
