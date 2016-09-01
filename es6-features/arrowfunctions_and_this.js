a8c6450546b4265a2fc4c0939d847c4593630ee3
mcpengelly <pengelly.mat@gmail.com>
2016-09-01 18:30:01 -0400

    arrow functions in es6 versus es5 this scoping

---
+++ es6-features/arrowfunctions_and_this.js
@@ -0,0 +1,24 @@
+//arrow functions in es6 lexically bind the surrounding 'this' to the containing block
+var foot = {
+	kick: () => {
+		this.yelp = 'Ouch!';
+		setImmediate(() => {
+			console.log(this.yelp);
+		});
+	}
+};
+foot.kick();
+
+// for example the above code has the same behaviour as below
+//set 'that' to the current value of this ('this' will be undefined within the kick function)
+// var that = this;
+// var foot = {
+// 	kick: function() {
+// 		that.yelp = 'Ouch!';
+// 		setImmediate(function() {
+// 			console.log(that.yelp);
+// 		});
+// 	}
+// };
+// foot.kick();
+
