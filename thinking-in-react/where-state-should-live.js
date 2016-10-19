ca6421f2eda9848a5bbea9027f936bd0d293494a
mcpengelly <pengelly.mat@gmail.com>
2016-10-18 21:06:08 -0400

    start test-anythig

---
+++ thinking-in-react/where-state-should-live.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+// filterText
+// inStockOnly
+
+export const FilterableProductTable = React.createClass({
+		render() {
+			var products = this.props.products;
+
+				return (
+					<div>
+						<SearchBar/>
+						<ProductTable products={products}/>
+					</div>
+				)
+		}
+})
+
+export const SearchBar = React.createClass({
+	render() {
+		return (
+			<form>
+				<input type='search' placeholder='search...'></input>
+				<label>
+				<input type='checkbox'></input>
+					only show products in stock
+				</label>
+			</form>
+		)
+	}
+})
+
+export const ProductTable = React.createClass({
+		render() {
+			const products = this.props.products;
+			const rows = [];
+			let currentCategory;
+
+			//each product gets it own ProductCategoryRow and Product row
+			products.forEach((product) => {
+	      if (product.category !== currentCategory) {
+          currentCategory = product.category;
+
+	        rows.push((
+            <ProductCategoryRow key={currentCategory} category={currentCategory} />
+          ));
+	      }
+	      rows.push((
+					<ProductRow name={product.name} product={product} />
+				));
+			});
+			return (
+				<table>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Price</th>
+						</tr>
+					</thead>
+					<tbody>
+						{rows}
+					</tbody>
+				</table>
+			)
+		}
+})
+
+export const ProductCategoryRow = React.createClass({
+		render() {
+				return (
+					<tr><b>{this.props.category}</b></tr>
+				)
+		}
+})
+
+export const ProductRow = React.createClass({
+		render() {
+				return (
+					<tr>Product: {this.props.product.name} | Price: {this.props.product.price}</tr>
+				)
+		}
+})
