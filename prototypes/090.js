f2e6a5d57139f7542cfe60644f5319d3e34ec8fc
mcpengelly <pengelly.mat@gmail.com>
2016-10-25 20:08:41 -0400

    implicit this

--- prototypes/090.js
+++ prototypes/090.js
@@ -1,11 +1,16 @@
 // -> Define two constructor functions: 'Robot' and 'Vehicle'
-// -> When called with 'new', the Robot constructor function should return 
+// -> When called with 'new', the Robot constructor function should return
 //    the implicit 'this'
-// -> When called with 'new', the Vehicle constructor function should return 
+// -> When called with 'new', the Vehicle constructor function should return
 //    an object of your own making, not the implicit 'this'.
 
-__
+function Robot(){
+	return this;
+}
 
+function Vehicle(){
+	return {};
+}
 
 // ------------------------------------------------
 // Common JS exports for verification, don't modify
