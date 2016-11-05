9b6523a4e95c2472812d5cb4f5cd36609d0a26cb
mcpengelly <pengelly.mat@gmail.com>
2016-11-04 23:46:41 -0400

    basic components

--- youreact/re/components/views/index.jsx
+++ youreact/re/components/views/index.jsx
@@ -1,21 +1,33 @@
-   import React from 'react';
+	 import React from 'react';
 
-    export default class TodoBox extends React.Component {
-        render() {
-            return (
-                <div className="todoBox">
-                    <h1>Todos</h1>
-                    <TodoList />
-                    <TodoForm />
-                </div>
-            );
-        }
-    }
+		export default class TodoBox extends React.Component {
+				render() {
+						return (
+								<div className="todoBox">
+										<h1>Todos</h1>
+										<TodoList />
+										<TodoForm />
+								</div>
+						);
+				}
+		}
 
-    class TodoList extends React.Component {
-      // Write code here
-    }
+		class TodoList extends React.Component {
+			render() {
+				return (
+					<div className='todoList'>
+						I am a TodoList.
+					</div>
+				)
+			}
+		}
 
-    class TodoForm extends React.Component {
-      // Write code here
-    }
+		class TodoForm extends React.Component {
+			render() {
+				return (
+					<div className='todoForm'>
+						I am a TodoForm.
+					</div>
+				)
+			}
+		}
